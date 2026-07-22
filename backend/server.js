70ee0195d624920893840dc5990a7cf12e206187
EC2 Default User <ec2-user@ip-10-0-1-112.eu-north-1.compute.internal>
2026-07-22 08:05:20 +0000

    my first commit task5

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const recipeRoutes = require("./routes/recipes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/recipes", recipeRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
